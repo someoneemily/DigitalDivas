2adf9cbd75b305cb6927b744571028b9eae9a52d
someoneemily <iemily.cwx@gmail.com>
2016-10-29 08:41:29 -0500

    added media release form downloads for eng and span

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get 'release_form', to: 'home#release'
   root 'home#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
